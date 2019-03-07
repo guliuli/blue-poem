3642a5aa90402873c8440090ecb58029dae2ac1f
guliuli <1270724556@qq.com>
2019-03-07 17:32:44 +0800

    fixed

--- src/number.js
+++ src/number.js
@@ -2,6 +2,12 @@ function isNum(value) {
     return (typeof value === 'number') && !isNaN(parseFloat(value)) && isFinite(value); 
 }
 
+
+function isFinite(value) {
+    return (typeof value === 'number') && !isNaN(parseFloat(value)) && isFinite(value); 
+}
+
+
 module.exports = {
     isNum,
 }
