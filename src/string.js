aa1c6c13496b03e3df5b1227296127cee8b64f15
guliuli <1270724556@qq.com>
2019-03-07 16:55:14 +0800

    fixed

--- src/string.js
+++ src/string.js
@@ -1,6 +1,16 @@
 function isString(value) {
 	return typeof value === 'string' || value instanceof String;
 }
+
+
+function contains(source, value) {
+	if (source == undefined || value == undefined) {
+		return false
+	}
+	return source.indexOf(value) > -1;
+}
+
 module.exports = {
 	isString,
+	contains
 };
